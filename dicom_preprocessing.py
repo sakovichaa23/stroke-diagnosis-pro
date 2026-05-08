c522688a37cbbb3566d6b1323c8ef8450caef17b
anastasia <sakovich_aa_23@mf.grsu.by>
2026-05-08 18:31:09 +0300

    Update dicom_preprocessing.py

--- dicom_preprocessing.py
+++ dicom_preprocessing.py
@@ -1,7 +1,6 @@
 import cv2
 import numpy as np
 import pydicom
-from config import IMPORTANT_DICOM_TAGS
 
 def dicom_to_rgb(ds):
     try:
@@ -18,18 +17,3 @@ def dicom_to_rgb(ds):
     except Exception as e:
         print(f"Ошибка чтения DICOM: {e}")
         return np.zeros((512, 512, 3), dtype=np.uint8)
-
-def clean_num(val):
-    try:
-        if isinstance(val, (list, pydicom.multival.MultiValue)):
-            return ", ".join([f"{float(x):.3f}" for x in val])
-        s = str(val).replace('[','').replace(']','').split(',')[0]
-        return f"{float(s):.3f}".rstrip('0').rstrip('.')
-    except:
-        return str(val)
-
-def extract_meta(ds):
-    meta = {}
-    for tag in IMPORTANT_DICOM_TAGS:
-        meta[tag] = clean_num(getattr(ds, tag, "Н/Д"))
-    return meta
